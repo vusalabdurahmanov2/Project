de3ecc15f903cd83b2bb61e7e48ca116fe1587ce
vusalabdurahmanov2 <miningvusal17@gmail.com>
2022-03-14 18:44:54 +0400

    first commit

---
+++ Gun/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Gun.model;
+namespace Gun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Daragin gulle tutmunu daxil edin:");
+            int WeaponBullet = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("daragdaki gulle sayini daxil edin:");
+            int CurrentBullet = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Daragin bosalma saniyesi daxil edin:");
+            int CombOfTheWeaponEmpty = Convert.ToInt32(Console.ReadLine());
+            Weapon gun1 = new Weapon(WeaponBullet, CurrentBullet, CombOfTheWeaponEmpty);
+            gun1.Fire();
+            gun1.Shoot();
+        }
+    }
+}
---
+++ Gun/model/Weapon.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gun.model
+{
+
+    class Weapon
+    {
+        public int WeaponBullet;//daragin gulle tutumu
+        public int CurrentBullet;//daragdaki gulle sayisi
+        public double CombOfTheWeaponEmpty;//daragin bosalma saniyesi
+        public Weapon(int WeaponBullet, int CurrentBullet, double CombOfTheWeaponEmpty)
+        {
+            this.WeaponBullet = WeaponBullet;
+            this.CurrentBullet = CurrentBullet;
+            this.CombOfTheWeaponEmpty = CombOfTheWeaponEmpty;
+        }
+        public void Shoot()
+        {
+            if (CurrentBullet > 0)
+            {
+                CurrentBullet -= 1;
+                Console.WriteLine("BOOOM!!One Bullet Shoted");
+            }
+            else
+            {
+                Console.WriteLine("You Must Reload Gun");
+            }
+            
+
+        }
+        public void Fire()
+        {
+            double BulletEmptyTime = 0;
+            double OneBulletShotTime = CombOfTheWeaponEmpty / CurrentBullet;//1 gullenin boslam saniye
+            while (CurrentBullet > 0)
+            {  
+                CurrentBullet -= 1;
+                BulletEmptyTime = BulletEmptyTime + OneBulletShotTime;
+            }
+            Console.WriteLine($"time:{BulletEmptyTime}");
+        }
+        public int GetRemainBulletCount()
+        {
+            return WeaponBullet- CurrentBullet;
+        }
+        public void Reload()
+        {
+            Console.WriteLine(WeaponBullet - CurrentBullet);
+            Console.WriteLine($"{WeaponBullet - CurrentBullet} Gulle dolduruldu");
+            CurrentBullet = WeaponBullet;
+        }
+    }
+}
