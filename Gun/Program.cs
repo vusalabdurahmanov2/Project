3a4a88b579d1e8b216b420a93db3b2d6cbdd6af5
vusalabdurahmanov2 <miningvusal17@gmail.com>
2022-03-15 19:17:43 +0400

    initial commet

--- Gun/Program.cs
+++ Gun/Program.cs
@@ -6,15 +6,47 @@ namespace Gun
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Daragin gulle tutmunu daxil edin:");
+            Console.Write("Daragin gulle tutmunu daxil edin:");
             int WeaponBullet = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("daragdaki gulle sayini daxil edin:");
+            Console.Write("daragdaki gulle sayini daxil edin:");
             int CurrentBullet = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Daragin bosalma saniyesi daxil edin:");
-            int CombOfTheWeaponEmpty = Convert.ToInt32(Console.ReadLine());
-            Weapon gun1 = new Weapon(WeaponBullet, CurrentBullet, CombOfTheWeaponEmpty);
-            gun1.Fire();
-            gun1.Shoot();
+            Console.Write("Daragin bosalma saniyesi daxil edin:");
+            int GunMagazine = Convert.ToInt32(Console.ReadLine());
+            Weapon gun1 = new Weapon(WeaponBullet, CurrentBullet, GunMagazine);     
+            int key = 0;
+            do
+            {
+                Console.WriteLine("Methodlardan birini secin\n");
+                key = Convert.ToInt32(Console.ReadLine());
+                switch (key)
+                {
+                    case 1:
+                        gun1.Shoot();
+                        break;
+                    case 2:
+                        gun1.Reload();
+                        break;
+                    case 3:
+                        gun1.Fire();
+                        break;
+                    case 4:
+                        Console.WriteLine("4cu metod");
+                        break;
+                    case 5:
+                        Console.WriteLine("5ci  metod");
+                        break;
+                    case 6:
+                        Console.WriteLine("Sagolun");
+                        break;
+                    case 0:
+                        Console.WriteLine("0ci  metod");
+                        break;
+                    default:
+                        Console.WriteLine("Yanis daxil etdiniz");
+                        break;
+                }
+            }while (key != 6);
+            
         }
     }
 }
--- Gun/model/Weapon.cs
+++ Gun/model/Weapon.cs
@@ -6,34 +6,53 @@ namespace Gun.model
 {
 
     class Weapon
-    {
-        public int WeaponBullet;//daragin gulle tutumu
+    {   public int WeaponBullet;//daragin gulle tutumu
         public int CurrentBullet;//daragdaki gulle sayisi
-        public double CombOfTheWeaponEmpty;//daragin bosalma saniyesi
-        public Weapon(int WeaponBullet, int CurrentBullet, double CombOfTheWeaponEmpty)
+        public double GunMagazine;//daragin bosalma saniyesi
+        public Weapon(int WeaponBullet, int CurrentBullet, double GunMagazine)
         {
-            this.WeaponBullet = WeaponBullet;
+            if (WeaponBullet > 0) this.WeaponBullet = WeaponBullet; this.WeaponBullet = WeaponBullet;
+            while (WeaponBullet <= 0)
+            {
+                Console.Write("Daragin gulle tutumunu dogru daxil edin:");
+                WeaponBullet = Convert.ToInt32(Console.ReadLine());
+            }
+            while (WeaponBullet< CurrentBullet)
+            {
+                Console.WriteLine("cari gulle sayi daragdaki gulle tutumdan cox ola bilmez");
+                Console.Write("Daragin gulle tutumunu dogru daxil edin:");
+                WeaponBullet = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Daragin gulle sayisni dogru daxil edin:");
+                CurrentBullet = Convert.ToInt32(Console.ReadLine());
+            }
             this.CurrentBullet = CurrentBullet;
-            this.CombOfTheWeaponEmpty = CombOfTheWeaponEmpty;
+            if (GunMagazine>0) this.GunMagazine = GunMagazine;
+            while(GunMagazine <= 0)
+            {
+                Console.Write("Daragin bosalmas saniyesini dogru daxil edin:");
+                GunMagazine = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public void  IsNegative()
+        {
+
         }
         public void Shoot()
         {
             if (CurrentBullet > 0)
             {
                 CurrentBullet -= 1;
-                Console.WriteLine("BOOOM!!One Bullet Shoted");
+                Console.WriteLine("BOOOM!!");
             }
             else
             {
                 Console.WriteLine("You Must Reload Gun");
             }
-            
-
         }
         public void Fire()
         {
             double BulletEmptyTime = 0;
-            double OneBulletShotTime = CombOfTheWeaponEmpty / CurrentBullet;//1 gullenin boslam saniye
+            double OneBulletShotTime = GunMagazine / CurrentBullet;//1 gullenin boslam saniye
             while (CurrentBullet > 0)
             {  
                 CurrentBullet -= 1;
